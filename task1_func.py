b6527488dd8a5dae25d7c88dd5a6521ea3ede31e
amitapr1982 <110875110+amitapr1982@users.noreply.github.com>
2025-12-29 16:32:04 -0600

    Add files via upload

---
+++ task1_func.py
@@ -0,0 +1,11 @@
+
+number = int(input("Enter a number: "))
+
+def factorial(number):
+    result = 1
+
+    for i in range(1, number + 1):
+        result *= i
+    return result
+
+print(f"Factorial of {number} is {factorial(number)}")
