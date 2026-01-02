92924774f048fd3daa6c5b45a3f0caf9e9b74fd1
amitapr1982 <110875110+amitapr1982@users.noreply.github.com>
2026-01-02 11:14:38 -0600

    Add files via upload

---
+++ task2_mod.py
@@ -0,0 +1,18 @@
+import math
+
+number = float(input("Enter a number: "))
+
+# Calculate the Square root of the number
+square_root= math.sqrt(number)
+
+# Calculate Natural logarithm (log base e) of the number
+natural_num = math.log(number)
+
+#Calculate Sine of the number (in radians)
+sine_num = math.sin(number)
+
+print(f"Square root: {square_root}")
+print(f"Natural number: {natural_num}")
+print(f"Sine number: {sine_num}")
+
+
